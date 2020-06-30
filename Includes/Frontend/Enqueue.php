f505c90a935205c43cb927ba2176b65fc525d723
Ankit Gade <ankit.gade@news.co.uk>
2020-06-30 12:20:40 +0530

    Load scripts and styles only on cart and accounts pages

--- Includes/Frontend/Enqueue.php
+++ Includes/Frontend/Enqueue.php
@@ -62,27 +62,33 @@ class Enqueue {
 	 * We will use asset-manifest.json to load the js/css files.
 	 */
 	public function load_scripts() {
-		$settings                            = [];
-		$settings['socials']                 = $this->util->plugin()->settings()->active_social_medias();
-		$settings['nonce']                   = is_user_logged_in() ? wp_create_nonce( 'wp_rest' ) : '';
-		$settings['wcssc_nonce']             = wp_create_nonce( 'wcssc_api' );
-		$settings['wcssc_btn_text']          = $this->util->plugin()->settings()->get( 'wcssc_btn_txt' );
-		$settings['wcssc_captcha_key']       = $this->util->plugin()->settings()->get( 'wcssc_captcha_key' );
-		$settings['api_path']['get_link']    = home_url( rest_get_url_prefix() . '/wcssc/v1/get-link' );
-		$settings['api_path']['save_cart']   = home_url( rest_get_url_prefix() . '/wcssc/v1/save-cart' );
-		$settings['api_path']['email_cart']  = home_url( rest_get_url_prefix() . '/wcssc/v1/email-cart' );
-		$settings['api_path']['saved_carts'] = home_url( rest_get_url_prefix() . '/wp/v2/wcssc-cart/' );
-		$settings['api_path']['delete_cart'] = home_url( rest_get_url_prefix() . '/wp/v2/wcssc-cart/{id}' );
+		if( is_cart() || is_account_page() ) {
 
-		/**
-		 * Enqueue font awesome script
-		 */
-		wp_enqueue_style( 'wcssc-font-awesome', 'https://use.fontawesome.com/releases/v5.12.1/css/all.css', array(), false );
-		wp_enqueue_style( 'wcssc', WCSSC_ASSETS_BUILD . '/main.css' );
-		wp_enqueue_script( 'wcssc-main', WCSSC_ASSETS_BUILD . '/index.js', [ 'wp-data', 'wp-core-data', 'wp-i18n' ], null, true );
-		wp_set_script_translations( 'wcssc-main', 'wcssc', WCSSC_BASE . '/languages/' . get_locale() );
-		wp_localize_script( 'wcssc-main', 'wcssc_settings', $settings );
+			$settings                            = [];
+			$settings['socials']                 = $this->util->plugin()->settings()->active_social_medias();
+			$settings['nonce']                   = is_user_logged_in() ? wp_create_nonce( 'wp_rest' ) : '';
+			$settings['wcssc_nonce']             = wp_create_nonce( 'wcssc_api' );
+			$settings['wcssc_btn_text']          = $this->util->plugin()->settings()->get( 'wcssc_btn_txt' );
+			$settings['wcssc_captcha_key']       = $this->util->plugin()->settings()->get( 'wcssc_captcha_key' );
+			$settings['api_path']['get_link']    = home_url( rest_get_url_prefix() . '/wcssc/v1/get-link' );
+			$settings['api_path']['save_cart']   = home_url( rest_get_url_prefix() . '/wcssc/v1/save-cart' );
+			$settings['api_path']['email_cart']  = home_url( rest_get_url_prefix() . '/wcssc/v1/email-cart' );
+			$settings['api_path']['saved_carts'] = home_url( rest_get_url_prefix() . '/wp/v2/wcssc-cart/' );
+			$settings['api_path']['delete_cart'] = home_url( rest_get_url_prefix() . '/wp/v2/wcssc-cart/{id}' );
 
+			/**
+			 * Enqueue font awesome script
+			 */
+			wp_enqueue_style( 'wcssc-font-awesome', 'https://use.fontawesome.com/releases/v5.12.1/css/all.css', array(), false );
+			wp_enqueue_style( 'wcssc', WCSSC_ASSETS_BUILD . '/main.css' );
+			wp_enqueue_script( 'wcssc-main', WCSSC_ASSETS_BUILD . '/index.js', [
+				'wp-data',
+				'wp-core-data',
+				'wp-i18n'
+			], null, true );
+			wp_set_script_translations( 'wcssc-main', 'wcssc', WCSSC_BASE . '/languages/' . get_locale() );
+			wp_localize_script( 'wcssc-main', 'wcssc_settings', $settings );
+		}
 	}
 
 }
